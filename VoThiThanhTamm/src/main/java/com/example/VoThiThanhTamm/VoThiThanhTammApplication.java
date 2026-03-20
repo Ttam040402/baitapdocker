7d6267863c76712af9009e2013930e64243ad394
Ttam040402 <vothithanhtam.020404@gmail.com>
2026-03-20 10:48:55 +0700

    first commit

---
+++ VoThiThanhTamm/src/main/java/com/example/VoThiThanhTamm/VoThiThanhTammApplication.java
@@ -0,0 +1,13 @@
+package com.example.VoThiThanhTamm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoThiThanhTammApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoThiThanhTammApplication.class, args);
+	}
+
+}
---
+++ VoThiThanhTamm/src/test/java/com/example/VoThiThanhTamm/VoThiThanhTammApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VoThiThanhTamm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoThiThanhTammApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
